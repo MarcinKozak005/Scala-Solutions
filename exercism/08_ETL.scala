404bec6a3cf342d8b8ed354fe42c7255100a301d
Marcin Kozak <marcinkozak005@gmail.com>
2022-12-29 22:18:15 +0100

    Add exercism:08_ETL

---
+++ exercism/08_ETL.scala
@@ -0,0 +1,13 @@
+import scala.collection.mutable
+
+object Etl {
+  def transform(scoreMap: Map[Int, Seq[String]]): Map[String, Int] = {
+    val result = mutable.Map[String,Int]()
+    for (pair <- scoreMap){
+      for (letter <- pair._2) {
+        result(letter.toLowerCase) = pair._1
+      }
+    }
+    result.toMap
+  }
+}
