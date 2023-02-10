e7f3e42b5be57ff2a1c0b30bd4e2f88d942f8127
Marcin Kozak <marcinkozak005@gmail.com>
2023-02-10 19:03:13 +0100

    Add SimplProgramProbl:ListsStrings:17MergeSort

--- Simple_Programming_Problems/src/main/scala/P02_ListsStrings/P17Sortings.scala
+++ Simple_Programming_Problems/src/main/scala/P02_ListsStrings/P17Sortings.scala
@@ -1,6 +1,6 @@
 package P02_ListsStrings
 
-object P17SelectionSort {
+object P17Sortings {
   def P17ListSelectionSort(list: List[Int]): List[Int] = list match {
     case Nil => Nil
     case _ =>
@@ -25,9 +25,28 @@ object P17SelectionSort {
     result
   }
 
+  def P17ListMergeSort(list: List[Int]): List[Int] = {
+    def merge(left: List[Int], right: List[Int]): List[Int] = (left, right) match {
+      case (_, Nil) => left
+      case (Nil, _) => right
+      case (l :: lTail, r :: rTail) => if (l < r) l :: merge(lTail, right) else r :: merge(left, rTail)
+    }
+
+    def divide(list: List[Int]): List[Int] = list match {
+      case Nil => Nil
+      case _ :: Nil => list
+      case _ =>
+        val left = list.slice(0, list.length / 2)
+        val right = list.slice(list.length / 2, list.length)
+        merge(divide(left), divide(right))
+    }
+
+    divide(list)
+  }
+
   def main(args: Array[String]): Unit = {
     println(f"P17 L SelectionSort: ${P17ListSelectionSort(List(5, 3, 2, 4, 1))}")
     println(f"P17 L InsertionSort: ${P17ListInsertionSort(List(3, 2, 1, 32, 11, 23))}")
-
+    println(f"P17 L MergeSort: ${P17ListMergeSort(List(3, 2, 1, 32, 11, 23))}")
   }
 }
