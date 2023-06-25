3833dcceed0f7c49c3d9e949bcbc215c37b81bf6
Marcin Kozak <marcinkozak005@gmail.com>
2023-06-25 21:00:44 +0200

    Add P03:P03 Graph edges operations WIP

--- Simple_Programming_Problems/src/main/scala/P03_Intermediate/P03Graph.scala
+++ Simple_Programming_Problems/src/main/scala/P03_Intermediate/P03Graph.scala
@@ -1,83 +1,111 @@
 package P03_Intermediate
 
+import P03_Intermediate.Graph.{Edge, Vertex}
+
+import scala.annotation.tailrec
+
 class P03Graph extends App {
 }
 
 
-class Graph private(_values: Map[String, Option[Int]],
-                    _inEdges: Map[String, List[Edge]],
-                    _outEdges: Map[String, List[Edge]]) {
-  type Vertex = String
-  private val vertices = _values
-  private val inEdges = _inEdges
-  private val outEdges = _outEdges
-
-  def addVertex(name: Vertex, value: Option[Int] = None): Graph =
-    if (vertices.keySet.contains(name)) this
-    else Graph(
+class Graph private(vertices: Map[Vertex, Option[Int]],
+                    edges: Map[Edge, Option[Int]],
+                    inEdges: Map[Vertex, Set[Edge]],
+                    outEdges: Map[Vertex, Set[Edge]],
+                    toVertices: Map[Edge, Set[Vertex]],
+                    fromVertices: Map[Edge, Set[Vertex]],
+                   ) {
+  def setVertex(name: Vertex, value: Option[Int] = None): Graph =
+    Graph(
       vertices.updated(name, value),
-      inEdges.updated(name, List[Edge]()),
-      outEdges.updated(name, List[Edge]()),
+      edges,
+      inEdges.updated(name, Set[Edge]()),
+      outEdges.updated(name, Set[Edge]()),
+      toVertices,
+      fromVertices,
     )
 
   def getVertexValue(name: Vertex): Option[Int] = vertices.getOrElse(name, None)
 
-  def updateVertexValue(name: Vertex, value: Option[Int]): Graph =
-    Graph(vertices.updated(name, value), inEdges, outEdges)
+  def removeVertex(name: Vertex): Graph =
+    Graph(
+      vertices.removed(name),
+      edges,
+      inEdges.removed(name),
+      outEdges.removed(name),
+      toVertices,
+      fromVertices,
+    )
 
-  def removeVertex(name: Vertex): Graph = Graph(
-    vertices.removed(name),
-    inEdges.removed(name),
-    outEdges.removed(name)
-  )
+  def setDirectedConnection(from: Vertex, to: Vertex, edge: Edge, value: Option[Int] = None): Graph =
+    addDirectedConnectionHelper(from, to, s"d_$edge", value)
 
-  def addDirectedConnection(from: Vertex, to: Vertex, value: Option[Int] = None): Graph =
+  def setUndirectedConnection(v1: Vertex, v2: Vertex, edge: Edge, value: Option[Int] = None): Graph =
     this
-      .addVertex(from)
-      .addVertex(to)
-      .addOutEdge(from, to, value)
-      .addInEdge(from, to, value)
+      .addDirectedConnectionHelper(v1, v2, s"u_$edge", value)
+      .addDirectedConnectionHelper(v2, v1, s"u_$edge", value)
 
-  def addUndirectedConnection(v1: Vertex, v2: Vertex, value: Option[Int] = None): Graph =
-    this
-      .addDirectedConnection(v1, v2, value)
-      .addDirectedConnection(v2, v1, value)
-
-  private def addInEdge(from: Vertex, to: Vertex, value: Option[Int] = None) = Graph(
+  def removeEdge(edge: Edge): Graph = Graph(
     vertices,
-    inEdges.updated(to, Edge(from, to, value) :: inEdges.getOrElse(to, List())),
+    edges.removed(edge),
+    removeEdgeFromVertices(inEdges, toVertices.getOrElse(edge, Set[Vertex]()).toList, edge),
+    removeEdgeFromVertices(outEdges, fromVertices.getOrElse(edge, Set[Vertex]()).toList, edge),
+    toVertices.removed(edge),
+    fromVertices.removed(edge),
+  )
+  @tailrec
+  private def removeEdgeFromVertices(collection: Map[Vertex, Set[Edge]], verticesList: List[Vertex], edge: Edge): Map[Vertex, Set[Edge]] = verticesList match {
+    case v::tail => removeEdgeFromVertices(collection.updated(v,collection.getOrElse(v,Set[Edge]()).excl(edge)),tail,edge)
+    case Nil => collection
+  }
+
+  private def addDirectedConnectionHelper(from: Vertex, to: Vertex, edge: Edge, value: Option[Int]): Graph = {
+    this
+      .setVertex(from, getVertexValue(from))
+      .setVertex(to, getVertexValue(to))
+      .addOutEdge(from, edge, value)
+      .addInEdge(to, edge, value)
+  }
+
+  private def addInEdge(to: Vertex, edge: Edge, value: Option[Int] = None) = Graph(
+    vertices,
+    edges.updated(edge, value),
+    inEdges.updated(to, edge :: inEdges.getOrElse(to, Set())),
     outEdges,
+    toVertices.updated(edge, to :: toVertices.getOrElse(edge, Set())),
+    fromVertices,
   )
 
-  private def addOutEdge(from: Vertex, to: Vertex, value: Option[Int] = None) = Graph(
+  private def addOutEdge(from: Vertex, edge: Edge, value: Option[Int] = None) = Graph(
     vertices,
+    edges.updated(edge, value),
     inEdges,
-    outEdges.updated(from, Edge(from, to, value) :: outEdges.getOrElse(from, List())),
+    outEdges.updated(from, from :: outEdges.getOrElse(from, Set())),
+    toVertices,
+    fromVertices.updated(edge, from :: fromVertices.getOrElse(edge, Set())),
   )
 
 }
 
 object Graph {
   type Vertex = String
+  type Edge = String
 
   def apply() =
     new Graph(
       Map[Vertex, Option[Int]](),
-      Map[Vertex, List[Edge]](),
-      Map[Vertex, List[Edge]](),
+      Map[Edge, Option[Int]](),
+      Map[Vertex, Set[Edge]](),
+      Map[Vertex, Set[Edge]](),
+      Map[Edge, Set[Vertex]](),
+      Map[Edge, Set[Vertex]](),
     )
 
-  def apply(values: Map[String, Option[Int]],
-            inEdges: Map[String, List[Edge]],
-            outEdges: Map[String, List[Edge]],
-           ) =
-    new Graph(values, inEdges, outEdges)
-}
-
-private class Edge private(val from: String, val to: String, val value: Option[Int]) {}
-
-object Edge {
-  type Vertex = String
-
-  def apply(from: Vertex, to: Vertex, value: Option[Int]): Edge = new Edge(from, to, value)
+  private def apply(vertices: Map[Vertex, Option[Int]],
+                    edges: Map[Edge, Option[Int]],
+                    inEdges: Map[Vertex, Set[Edge]],
+                    outEdges: Map[Vertex, Set[Edge]],
+                    toVertices: Map[Edge, Set[Vertex]],
+                    fromVertices: Map[Edge, Set[Vertex]]) =
+    new Graph(vertices, edges, inEdges, outEdges, toVertices, fromVertices)
 }
